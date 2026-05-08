f7b4916ce7a0f3375c6b935a913d260f9f806ca4
thomas86renault-ui <thomas86renault@gmail.com>
2026-05-08 19:44:44 +0200

    Update database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -4,9 +4,11 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   max: 20,
   idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionTimeoutMillis: 10000,
+  ssl: { rejectUnauthorized: false },
+  keepAlive: true,
+  keepAliveInitialDelayMillis: 10000,
 });
-
 pool.on('error', (err) => {
   console.error('Unexpected error on idle client', err);
   process.exit(-1);
